41e18e28c3984dbc81a418cb81ac151c3b6eac29
Aisya Sofiyyah <92282227+aisyasofiyyah@users.noreply.github.com>
2022-07-23 14:13:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-#import matplotlib.pyplot as plt
-#from sklearn.linear_model import LinearRegression
-#from sklearn.model_selection import train_test_split
-#from sklearn.metrics import mean_squared_error, r2_score
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
 
 st.set_page_config(layout="wide")
 st.markdown("<h1 style='text-align: center;'>Medical Costs Prediction App</h1>", unsafe_allow_html=True)
@@ -20,53 +19,40 @@ def user_input_features():
 df = user_input_features()
 
 costs = pd.read_csv('https://raw.githubusercontent.com/aisyasofiyyah/insurance-costs/main/insurance.csv')
-X = costs.drop('charges', axis=1)
-Y = costs.charges
 
-clf= RandomForestClassifier()
-clf.fit(X,Y)
+model= LinearRegression()
+X1= pd.DataFrame(costs['bmi'])
+y1= costs['charges']
+X_train, X_test, y_train, y_test = train_test_split(X1, y1, test_size = 0.25, random_state = 0)
 
-prediction = clf.predict(df)
-prediction_proba = clf.predict_proba(df)
+model.fit(X_train,y_train)
+y1_pred= model.predict(X_test)
+st.write("Root mean squared error: {} ".format(mean_squared_error(y_test, y_pred)**0.5))
+st.write('Variance score: {} '.format(r2_score(y_test,y_pred)))
 
-st.write(df)
-st.write(prediction)
-st.write(prediction_proba)
+plt.scatter(X_test, y_test, color='black')
+plt.plot(X_test, y1_pred, color='blue', linewidth=1)
+plt.xlabel("BMI")
+plt.ylabel("Charges")
 
-#X1= pd.DataFrame(df['bmi'])
-#y1= df['charges']
-#X_train, X_test, y_train, y_test = train_test_split(X1, y1, test_size = 0.25, random_state = 0)
-
-#model= LinearRegression()
-#model.fit(X_train,y_train)
-#y1_pred= model.predict(X_test)
-#st.write("Root mean squared error: {} ".format(mean_squared_error(y_test, y_pred)**0.5))
-#st.write('Variance score: {} '.format(r2_score(y_test,y_pred)))
-
-#plt.scatter(X_test, y_test, color='black')
-#plt.plot(X_test, y1_pred, color='blue', linewidth=1)
-#plt.xlabel("BMI")
-#plt.ylabel("Charges")
-
-#plt.title('Charges vs BMI')
-#plt.show()
+plt.title('Charges vs BMI')
+plt.show()
 
 #finding correlation between age and costs
-#X2= pd.DataFrame(df['age'])
-#y2= df['charges']
-#X_train,X_test,y_train,y_test=train_test_split(X2,y2,test_size=0.25,random_state=0)
+X2= pd.DataFrame(costs['age'])
+y2= costs['charges']
+X_train,X_test,y_train,y_test=train_test_split(X2,y2,test_size=0.25,random_state=0)
 
-#model = LinearRegression(fit_intercept=True)
+model = LinearRegression(fit_intercept=True)
+model.fit(X_train, y_train) 
+y2_pred = model.predict(X_test)
+st.write(f"RMSE: {(mean_squared_error(y_test, y2_pred))**0.5}.")
+st.write(f"R^2: {r2_score(y_test, y2_pred):.4f}")
 
-#model.fit(X_train, y_train) 
-#y2_pred = model.predict(X_test)
-#st.write(f"RMSE: {(mean_squared_error(y_test, y2_pred))**0.5}.")
-#st.write(f"R^2: {r2_score(y_test, y2_pred):.4f}")
+plt.scatter(X_test, y_test,  color='black')
+plt.plot(X_test, y2_pred, color='blue', linewidth=1)
+plt.xlabel("Age")
+plt.ylabel("Charges")
 
-#plt.scatter(X_test, y_test,  color='black')
-#plt.plot(X_test, y2_pred, color='blue', linewidth=1)
-#plt.xlabel("Age")
-#plt.ylabel("Charges")
-
-#plt.title('Charges vs Age')
-#plt.show()
+plt.title('Charges vs Age')
+plt.show()
