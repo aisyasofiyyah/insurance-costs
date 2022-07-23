d5e9061f7bea912c4e79ac8f099af233575d8ff6
Aisya Sofiyyah <92282227+aisyasofiyyah@users.noreply.github.com>
2022-07-23 16:28:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,30 +9,36 @@ from sklearn.metrics import mean_squared_error, r2_score
 st.set_page_config(layout="wide")
 st.markdown("<h1 style='text-align: center;'>Medical Costs Prediction App</h1>", unsafe_allow_html=True)
 
-costs = pd.read_csv('https://raw.githubusercontent.com/aisyasofiyyah/insurance-costs/main/insurance.csv')
-
 option = st.sidebar.selectbox(
     'Select Variables',
      ['age','bmi','children','sweetviz'])
+     
+def read_date():
+    return pd.read_csv('https://raw.githubusercontent.com/aisyasofiyyah/insurance-costs/main/insurance.csv')[['age','bmi','children']]
 
-if option=='age':    
+df=read_data()
+
+if option=='age':
+    st.header("Modeling")
     X2= pd.DataFrame(costs['age'])
     y2= costs['charges']
-    X_train,X_test,y_train,y_test=train_test_split(X2,y2,test_size=0.25,random_state=0)
+    X_train, X_test, y_train, y_test=train_test_split(X2,y2,test_size=0.25,random_state=0)
 
     model = LinearRegression(fit_intercept=True)
     model.fit(X_train, y_train) 
     y2_pred = model.predict(X_test)
-    st.write(f"RMSE: {(mean_squared_error(y_test, y2_pred))**0.5}.")
-    st.write(f"R^2: {r2_score(y_test, y2_pred):.4f}")
+       
+    st.markdown(f"""Linear Regerssion model trained:
+            -RMSE:{(mean_squared_error(y_test, y2_pred))**0.5}
+            -Variance, r^2:{r2_score(y_test, y2_pred):.4f}
+    """)
     
     f=plt.figure()
     plt.scatter(X_test, y_test, color='black')
     plt.plot(X_test, y2_pred, color='blue', linewidth=1)
     plt.xlabel("Charges")
     plt.ylabel("Age")
-    plt.title('Age vs Charges')
-    
+    plt.title('Age vs Charges')    
     st.plotly_chart(f)
 
 elif option=='bmi':
