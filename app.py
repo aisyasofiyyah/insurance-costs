85e55d18cfaddc52912bd504ea339011a9612467
Aisya Sofiyyah <92282227+aisyasofiyyah@users.noreply.github.com>
2022-07-24 14:31:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -40,12 +40,13 @@ if option=='age':
             'Variance':{r2_score(y_test, y2_pred)}
              })
     
-    f=plt.figure()
-    plt.scatter(X_test, y_test, color='black')
-    plt.plot(X_test, y2_pred, color='blue', linewidth=1)
+    f=plt.figure(figsize=(12,6))
+    plt.scatter(X_test, y_test, color='black', label='Age')
+    plt.plot(X_test, y2_pred, color='blue', label='Predicted Medical Costs', linewidth=1)
     plt.xlabel("Charges")
     plt.ylabel("Age")
     plt.title('Age vs Charges')    
+    plt.legend()
     st.write(f)
 
 elif option=='bmi':
