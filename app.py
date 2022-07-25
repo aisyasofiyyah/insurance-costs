cd1908ae3991eb4e34b6e2bec2c1266e2b8ef0dc
Aisya Sofiyyah <92282227+aisyasofiyyah@users.noreply.github.com>
2022-07-25 22:18:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_agg import RendererAgg
+import plotly.express as px
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
